4a26b1679ee64ceaef7e5ec6a984b9d110da55d9
pawan841204 <kpawan841204@gmail.com>
2025-06-22 12:20:08 +0530

    Initial commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,17 @@
+// src/utils/api.js
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://employee-backend-h6nk.onrender.com/api',
+  baseURL: 'https://employee-backend-2-o6u8.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
 export default API;
+
